75b36fd75416bff96bd5cc10f5041457b14a54c5
Shertas <129880050+shertas@users.noreply.github.com>
2025-12-03 13:55:21 +0100

    refactor(renderInGrid): return setFound and alert if not enough cards

--- src/modules/game/components/deck/deck-renderer.js
+++ src/modules/game/components/deck/deck-renderer.js
@@ -1,6 +1,14 @@
 import { renderCards } from "./card-renderer.js"
+import { existsASetOnTable } from "../../services/check-set.js"
 export async function renderInGrid(deck, startIndex, cardsNumber) {
     const cards = deck.slice(startIndex, startIndex + cardsNumber)
-    const newCards = await renderCards(cards)
-    return newCards
+    const cardsLeft = deck.length - (startIndex + cardsNumber)
+    if (cardsLeft < cardsNumber) {
+        alert("No hay suficientes cartas en el mazo.")
+    }
+    else {
+        const newCards = await renderCards(cards)
+        const setFound = existsASetOnTable()
+        return { newCards, setFound }
+    }
 }
