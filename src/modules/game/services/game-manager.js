b0d9088aaef50689e6cf13cfb2e49ee7e9982d85
Shertas <129880050+shertas@users.noreply.github.com>
2025-12-13 02:13:48 +0100

    refactor(add-cards): create addCards function and move from game-manager

--- src/modules/game/services/game-manager.js
+++ src/modules/game/services/game-manager.js
@@ -5,55 +5,54 @@ import { setRules } from "./set-rules.js"
 import { initCardSelection, unselectCards } from "../components/deck/card-selection.js"
 import { clickHelpButton } from "./help-button.js"
 import { resolveSet } from "./resolve-set.js"
-import { existsASetOnTable } from "./check-set.js"
 import { Scoreboard } from "./scoreboard.js"
 import { timerStart, countdown } from "./stopwatch.js"
-import { checkGameEnd, endGame } from "./end-game.js"
 import { saveScore } from "./save-score.js"
+import { addCards } from "./add-cards.js"
+
 
 const urlParams = new URLSearchParams(window.location.search)
 const level = parseInt(urlParams.get('level')) || 2
 const pve = urlParams.get('pve') === '1'
-let deck = (level === 1) ? generateEasyDeck() : generateDeck()
-const penalty = setRules(level)
+
 
 document.addEventListener("DOMContentLoaded", async () => {
+    let deck = (level === 1) ? generateEasyDeck() : generateDeck()
+    const penalty = setRules(level)
+    const opponentSpeed = pveRules(level, pve)
+
     level === 3 ? countdown() : timerStart()
+
     const add3CardsButton = document.getElementById("add3")
     const isSetButton = document.getElementById("isSet")
     const endGameButton = document.getElementById("endGame")
+
     const selectedCards = initCardSelection()
     const shuffledDeck = shuffleDeck(deck)
     const totalCards = shuffledDeck.length
-    console.log(totalCards)
-    let currentCardIndex = 0
-    let setFound = false
-    let gameEnded = false
-    let addCards = await renderInGrid(shuffledDeck, currentCardIndex, 12)
-    currentCardIndex += addCards
-    setFound = existsASetOnTable()
-    if (!setFound && currentCardIndex < totalCards) {
-        addCards = await renderInGrid(shuffledDeck, currentCardIndex, 3)
-        currentCardIndex += addCards
-        setFound = existsASetOnTable()
-        scoreboard.updateCardsRemaining(addCards)
+    const scoreboard = new Scoreboard(totalCards)
+
+    const gameState = {
+        shuffledDeck,
+        totalCards,
+        scoreboard,
+        currentCardIndex: 0,
+        setFound: false,
+        gameEnded: false,
+        level,
+        pve
     }
 
-    // Inicializar el marcador
-    const scoreboard = new Scoreboard(totalCards)
-    scoreboard.updateCardsRemaining(12) // Se han usado 12 cartas iniciales
+
+    await addCards(gameState, 12)
+
+
+
 
     // Event Listeners
     if (level !== 3) {
         add3CardsButton.addEventListener("click", async () => {
-            addCards = await renderInGrid(shuffledDeck, currentCardIndex, 3)
-            currentCardIndex += addCards
-            scoreboard.updateCardsRemaining(addCards) // Actualizar cartas usadas
-            setFound = existsASetOnTable()
-            gameEnded = checkGameEnd(currentCardIndex, totalCards, setFound)
-            if (gameEnded) {
-                saveScore(scoreboard, pve)
-            }
+            await add3Cards(gameState)
         })
     }
     isSetButton.addEventListener("click", async () => {
@@ -67,22 +66,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             if (isSet) {
                 // Es un SET correcto - sumar 1 punto
                 scoreboard.addSet(1)
-                addCards = await renderInGrid(shuffledDeck, currentCardIndex, 3)
-                currentCardIndex += addCards
-                scoreboard.updateCardsRemaining(addCards) // Actualizar cartas usadas
-                setFound = existsASetOnTable()
-                if (!setFound && currentCardIndex < totalCards) {
-                    addCards = await renderInGrid(shuffledDeck, currentCardIndex, 3)
-                    currentCardIndex += addCards
-                    setFound = existsASetOnTable()
-                }
-                gameEnded = checkGameEnd(currentCardIndex, totalCards, setFound)
-                if (gameEnded) {
-                    saveScore(scoreboard, pve, level)
-                }
-
-
-                return true
+                await addCards(gameState, 3)
             } else {
                 // No es un SET - contabilizar error y aplicar penalización según nivel
                 scoreboard.addError()
@@ -107,7 +91,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         const helpButton = document.getElementById("help")
         helpButton.addEventListener("click", () => {
 
-            clickHelpButton(setFound, selectedCards)
+            clickHelpButton(gameState.setFound, selectedCards)
         }
         )
     }
