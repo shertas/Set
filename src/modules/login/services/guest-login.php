fcaf97c99a976565abca51745573287f1a1782a1
Ana <anahernanzcarro@gmail.com>
2025-12-21 21:51:05 +0100

    Add pass for guests in case of error

--- src/modules/login/services/guest-login.php
+++ src/modules/login/services/guest-login.php
@@ -3,8 +3,9 @@
 session_start();
 
 // Iniciar sesión como invitado
-$_SESSION['usuario'] = 'invitado';
-$_SESSION['is_guest'] = true;
+$_SESSION['usuario']    = 'invitado';
+$_SESSION['is_guest']   = true;
+$_SESSION['pass']       = 'invitado';
 
 // Redirigir a la página de bienvenida
 header("Location: ../views/welcome.html");
